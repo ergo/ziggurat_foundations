292eda3d2494330f00934fe1822a99636f7f8b6c
Marcin Lulek <info@webreactor.eu>
2012-02-13 23:09:26 +0100

    use server_default instead of default

--- zf_alembic/versions/264049f80948_create_ordering_colu.py
+++ zf_alembic/versions/264049f80948_create_ordering_colu.py
@@ -14,7 +14,7 @@ from alembic import op
 import sqlalchemy as sa
 
 def upgrade():
-    op.add_column('resources', sa.Column('ordering', sa.Integer, nullable=False, default=0))
+    op.add_column('resources', sa.Column('ordering', sa.Integer, server_default='0', nullable=False))
 
 def downgrade():
     op.drop_column('resources', 'ordering')
