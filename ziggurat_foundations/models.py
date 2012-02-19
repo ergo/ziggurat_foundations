ffa596ed007b30ed611e10453cc07a8167f40902
Marcin Lulek <info@webreactor.eu>
2012-02-19 14:50:07 +0100

    use external id instead username for external identities

--- ziggurat_foundations/models.py
+++ ziggurat_foundations/models.py
@@ -379,20 +379,20 @@ class ExternalIdentityMixin(BaseModel):
         return sa.Column(sa.String(255), default=u'')
 
     @classmethod
-    def by_username_and_provider(cls, external_user_name, provider_name,
+    def by_external_id_and_provider(cls, external_id, provider_name,
                                  db_session=None):
         db_session = get_db_session(db_session)
         query = db_session.query(cls)
-        query = query.filter(cls.external_user_name == external_user_name)
+        query = query.filter(cls.external_id == external_id)
         query = query.filter(cls.provider_name == provider_name)
         return query.first()
 
     @classmethod
-    def user_by_username_and_provider(cls, external_user_name, provider_name,
+    def user_by_external_id_and_provider(cls, external_id, provider_name,
                                  db_session=None):
         db_session = get_db_session(db_session)
         query = db_session.query(cls.User)
-        query = query.filter(cls.external_user_name == external_user_name)
+        query = query.filter(cls.external_id == external_id)
         query = query.filter(cls.User.user_name == cls.local_user_name)
         query = query.filter(cls.provider_name == provider_name)
         return query.first()
