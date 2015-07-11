c4e876cdc0f5dfdb3fa26295c3185042669801bd
Marcin Lulek <info@webreactor.eu>
2015-07-11 23:43:18 +0200

    managers: cleanup __init__.py

--- ziggurat_foundations/models/managers/__init__.py
+++ ziggurat_foundations/models/managers/__init__.py
@@ -1,17 +1,2 @@
-import hashlib
-import random
-import string
-import six
-import sqlalchemy as sa
-
-
-from ziggurat_foundations.permissions import (ANY_PERMISSION,
-                                              ALL_PERMISSIONS,
-                                              PermissionTuple,
-                                              resource_permissions_for_users)
-
-from ziggurat_foundations.models import get_db_session
-
-
 class ModelManager(object):
     pass
