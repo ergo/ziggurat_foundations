5a89f695d0ad8f8b3a61a2a6a873d0e051fd0d0d
Marcin Lulek <info@webreactor.eu>
2015-07-14 15:03:13 +0200

    external identity: use model and proxy

--- ziggurat_foundations/models/services/external_identity.py
+++ ziggurat_foundations/models/services/external_identity.py
@@ -7,17 +7,17 @@ class ExternalIdentityService(ModelManager):
     def by_external_id_and_provider(cls, external_id, provider_name,
                                     db_session=None):
         db_session = get_db_session(db_session)
-        query = db_session.query(cls)
-        query = query.filter(cls.external_id == external_id)
-        query = query.filter(cls.provider_name == provider_name)
+        query = db_session.query(cls.model)
+        query = query.filter(cls.model.external_id == external_id)
+        query = query.filter(cls.model.provider_name == provider_name)
         return query.first()
 
     @classmethod
     def user_by_external_id_and_provider(cls, external_id, provider_name,
                                          db_session=None):
         db_session = get_db_session(db_session)
-        query = db_session.query(cls._ziggurat_models.User)
-        query = query.filter(cls.external_id == external_id)
-        query = query.filter(cls._ziggurat_models.User.user_name == cls.local_user_name)
-        query = query.filter(cls.provider_name == provider_name)
+        query = db_session.query(cls.models_proxy.User)
+        query = query.filter(cls.model.external_id == external_id)
+        query = query.filter(cls.model.models_proxy.User.user_name == cls.local_user_name)
+        query = query.filter(cls.model.provider_name == provider_name)
         return query.first()
