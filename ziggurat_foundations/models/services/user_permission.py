21357385a59897a9ca592a157176e8d6745a4567
Marcin Lulek <info@webreactor.eu>
2015-07-14 19:45:32 +0200

    user permission: fix class references

--- ziggurat_foundations/models/services/user_permission.py
+++ ziggurat_foundations/models/services/user_permission.py
@@ -8,6 +8,6 @@ class UserPermissionService(ModelManager):
     def by_user_and_perm(cls, user_id, perm_name, db_session=None):
         """ return by user and permission name"""
         db_session = get_db_session(db_session)
-        query = db_session.query(cls).filter(cls.user_id == user_id)
-        query = query.filter(cls.perm_name == perm_name)
+        query = db_session.query(cls.model).filter(cls.model.user_id == user_id)
+        query = query.filter(cls.model.perm_name == perm_name)
         return query.first()
