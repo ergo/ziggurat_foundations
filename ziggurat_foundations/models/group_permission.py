a1b0052ea4447daa5bb452326cc63b1432d20100
Marcin Lulek <info@webreactor.eu>
2015-07-14 15:05:16 +0200

    group permission: does not inherit from service anymore

--- ziggurat_foundations/models/group_permission.py
+++ ziggurat_foundations/models/group_permission.py
@@ -1,13 +1,16 @@
 import sqlalchemy as sa
 from sqlalchemy.ext.declarative import declared_attr
 from .base import BaseModel
-from .services.group_permission import GroupPermissionManager
+from .services.group_permission import GroupPermissionService
+from ..utils import get_db_session
 
 
-class GroupPermissionMixin(GroupPermissionManager, BaseModel):
+class GroupPermissionMixin(BaseModel):
     """ group permission mixin """
 
     __table_args__ = {'mysql_engine': 'InnoDB', 'mysql_charset': 'utf8'}
+
+    _ziggurat_service = GroupPermissionService
 
     @declared_attr
     def __tablename__(self):
@@ -25,3 +28,11 @@ class GroupPermissionMixin(GroupPermissionManager, BaseModel):
 
     def __repr__(self):
         return '<GroupPermission: %s>' % self.perm_name
+
+    @classmethod
+    def by_group_and_perm(cls, group_id, perm_name, db_session=None):
+        db_session = get_db_session(db_session)
+        return GroupPermissionService.by_group_and_perm(
+            group_id=group_id,
+            perm_name=perm_name,
+            db_session=db_session)
--- ziggurat_foundations/models/services/group_permission.py
+++ ziggurat_foundations/models/services/group_permission.py
@@ -2,11 +2,11 @@ from . import ModelManager
 from ...utils import get_db_session
 
 
-class GroupPermissionManager(ModelManager):
+class GroupPermissionService(ModelManager):
     @classmethod
     def by_group_and_perm(cls, group_id, perm_name, db_session=None):
         """" return by by_user_and_perm and permission name """
         db_session = get_db_session(db_session)
-        query = db_session.query(cls).filter(cls.group_id == group_id)
-        query = query.filter(cls.perm_name == perm_name)
+        query = db_session.query(cls.model).filter(cls.model.group_id == group_id)
+        query = query.filter(cls.model.perm_name == perm_name)
         return query.first()
