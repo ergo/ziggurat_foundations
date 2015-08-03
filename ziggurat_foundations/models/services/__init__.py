763638065067f5e6b3b52661c9b416789b6070f2
Marcin Lulek <info@webreactor.eu>
2015-08-03 11:55:14 +0200

    services: handle all() explictly

--- ziggurat_foundations/models/services/__init__.py
+++ ziggurat_foundations/models/services/__init__.py
@@ -1,7 +1,19 @@
+from ..base import get_db_session
+
 class BaseService(object):
     model = None
     model_proxy = None
 
     @classmethod
-    def all(cls, db_session=None):
-        return cls.model.base_query(db_session)
+    def all(cls, klass, db_session=None):
+        """
+        returns all objects of specific type - will work correctly with
+        sqlalchemy inheritance models, you should normally use models
+        base_query()  instead of this function its for bw. compat purposes
+
+        :param klass:
+        :param db_session:
+        :return:
+        """
+        db_session = get_db_session(db_session)
+        return db_session.query(klass)
