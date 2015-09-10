73c994ed831fc61261f3f5c36733cc2c50eddfcb
Luke Crooks <Luke@pumalo.org>
2015-09-10 10:34:55 +0100

    fixing groupfinder and adding documentation for it

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'ziggurat_foundations'
-copyright = u'2012, Marcin Lulek - Webreactor.eu'
+copyright = u'2012-2015, Marcin Lulek - Webreactor.eu'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
