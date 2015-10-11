b6b8d81e471ac306c37d047b7cbadea2db27f510
Marcin Lulek <info@webreactor.eu>
2015-10-11 23:15:57 +0200

    setup.py: accidently a comma

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         "sqlalchemy",
         "passlib>=1.6.1",
-        "bcrypt"
+        "bcrypt",
         "paginate",
         "paginate_sqlalchemy",
         "alembic",
