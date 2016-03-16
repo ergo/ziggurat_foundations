b75677d56e34cf17a468305bf3fc49846cc5730a
Marcin Lulek <info@webreactor.eu>
2016-03-16 23:50:50 +0100

    migrations: unbreak mysql migrations for 3cfc41...

--- ziggurat_foundations/migrations/versions/3cfc41c4a5f0_groups_pkey_change.py
+++ ziggurat_foundations/migrations/versions/3cfc41c4a5f0_groups_pkey_change.py
@@ -12,15 +12,46 @@ down_revision = '53927300c277'
 
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy.dialects.mysql.base import MySQLDialect
+from alembic.context import get_context
+from sqlalchemy.engine.reflection import Inspector
 
 
 def upgrade():
+    c = get_context()
+    if isinstance(c.connection.engine.dialect, MySQLDialect):
+        insp = Inspector.from_engine(c.connection.engine)
+        for t in ['groups_permissions', 'groups_resources_permissions',
+                  'users_groups', 'resources']:
+            for constraint in insp.get_foreign_keys(t):
+                if constraint['referred_columns'] == ['group_name']:
+                    op.drop_constraint(constraint['name'], t,
+                                       type='foreignkey')
+
     op.drop_column('groups', 'id')
     op.alter_column('groups', 'group_name',
                     type_=sa.String(128),
                     existing_type=sa.String(50),
                     )
     op.create_primary_key('groups_pkey', 'groups', cols=['group_name'])
+
+    if isinstance(c.connection.engine.dialect, MySQLDialect):
+        op.create_foreign_key(None, 'groups_permissions', 'groups',
+                              remote_cols=['group_name'],
+                              local_cols=['group_name'], onupdate='CASCADE',
+                              ondelete='CASCADE')
+        op.create_foreign_key(None, 'groups_resources_permissions', 'groups',
+                              remote_cols=['group_name'],
+                              local_cols=['group_name'], onupdate='CASCADE',
+                              ondelete='CASCADE')
+        op.create_foreign_key(None, 'users_groups', 'groups',
+                              remote_cols=['group_name'],
+                              local_cols=['group_name'], onupdate='CASCADE',
+                              ondelete='CASCADE')
+        op.create_foreign_key(None, 'resources', 'groups',
+                              remote_cols=['group_name'],
+                              local_cols=['owner_group_name'], onupdate='CASCADE',
+                              ondelete='SET NULL')
 
 
 def downgrade():
