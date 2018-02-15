5e005fab08da740a027dcc23ba1b53abc3efaec4
Marcin Lulek <info@webreactor.eu>
2018-02-15 11:54:36 +0100

    resource: add missing indices

---
+++ ziggurat_foundations/migrations/versions/613e7c11dead_create_indices_on_resource_owners.py
@@ -0,0 +1,21 @@
+"""create indices on resource owners
+
+Revision ID: 613e7c11dead
+Revises: b5e6dd3449dd
+Create Date: 2018-02-15 11:51:29.659352
+
+"""
+from __future__ import unicode_literals
+
+# revision identifiers, used by Alembic.
+revision = '613e7c11dead'
+down_revision = 'b5e6dd3449dd'
+
+from alembic import op
+
+def upgrade():
+    op.create_index(op.f('ix_resources_owner_group_id'), 'resources', ['owner_group_id'])
+    op.create_index(op.f('ix_resources_owner_user_id'), 'resources', ['owner_user_id'])
+
+def downgrade():
+    pass
